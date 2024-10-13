3371f4e80e82b77a42503854551218ca558984b4
StevenNguyen <115420486+NgMinhTie@users.noreply.github.com>
2024-10-13 14:56:46 +0000

    Add from Step 3 to Step 6

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Assignment_SE
 
+// FROM STEP 1 TO 5 IS HOW TO RETRIEVE DATA THROUGH GET
+
 STEP 1: Turn on PowerShell:
 - Type some command with "npm" to create "package.json" and "package-lock.json" files
 - Then add into "package.jon":
@@ -19,3 +21,28 @@ STEP 3: Define Route
     with: "/" is the path (endpoint) of the route that we need to access
     (resquest, response): request: from user to server, response: from server to user, they are the    callback function.
     => {}: they body code
+
+STEP 4: ROUTE PARAMS
+- This is the step we add the parameter(explicit) in the link in order to access the data
+- We have:
+    app.get('/api/users/:id' (resquest, respond) => {})
+    where ":id" is to access the user who has this id.
+- request.params.id (extract the value of the id in the request)
+
+STEP 5: QUERY PARAMS
+- The link will have the type: 
+    "localhost:3000/users?key=value&key2=value2"
+    we can add more key and value
+- Initial 2 variables "filter" and "value" from the "request".
+
+// USE TO CREATE DATA FROM THE POST REQUEST
+
+STEP 6: POST REQUESTS
+- Install Extension "Thunder Clienmt" and enter our own URL
+- Add the function app.post()
+- Use "Thunder Client" again => Change from "GET" to "POST" and add this script below:
+    {
+        "username": "anson"
+    }
+- Add "app.use(express.json())" after app.
+- Create variable "body" imported the data from "request".
--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json()); // This line is middleware use to analyse the body of the request and assign to the variable "body"
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (resquest, respond) => {
@@ -9,6 +10,37 @@ app.get("/", (resquest, respond) => {
     //respond.status(201).send({msg: "Hello"});
 });
 
+//Apply for one part of Step 4
+const mockUsers = [
+    {id: 1, username: "anson", displayName: "Anson"},
+    {id: 2, username: "jack", displayName: "Jack"},
+    {id: 3, username: "adam", displayName: "Adam"},
+    {id: 4, username: "tina", displayName: "Tina"},
+    {id: 5, username: "jason", displayName: "Json"},
+    {id: 6, username: "henry", displayName: "Henry"},
+    {id: 7, username: "marilyn", displayName: "Marilyn"},
+];
+
+app.get("/api/users", (request, response) => {
+    //QUERY PARAMS
+    console.log(request.query);
+    const {
+        query: {filter, value},
+    } = request;
+    // These three lines code above are the Destructing Assignment (which is used to query the data directly through request (which is currently a link))
+    // "query:" is used to extract the (filter and value) through the request link
+    if(!filter || !value) return response.send(mockUsers);
+    else if (filter && value) 
+        return response.send(mockUsers.filter((user) => user[filter].include(value)))
+    // "filter()" function is used to filter and it will return the array whose elements are true
+    // "user[filter].include(value)" is used to check whether it has the value if having it will return the value
+    // where "user[filter]" to check the attribute of the user. In this case, this one is "username"
+    // ".include(value)" to extract the value (string) of the user[filter]
+    // Ex: if value is "an", it can show "anson".
+
+})
+
+/** Apply from Step 1 to a part of Step 4
 app.get("/api/users", (resquest, response) => {
     response.send([
         {id: 1, username: "anson", displayName: "Anson"},
@@ -16,10 +48,39 @@ app.get("/api/users", (resquest, response) => {
         {id: 1, username: "adam", displayName: "Adam"},
     ]);
 });
+*/
 
 app.get("/api/products", (request, response) => {
     response.send([{id: 123, name:"chicken breast", price: 12.99}]);
 });
+
+//STEP 4: ROUTE PARAMAS
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params.id);  //http:/localhost/api/users/1 (1 is the "id")
+    const parseID = parseInt(request.params.id); // Convert the above id (which is string) to Integer 
+    console.log(parseID); // Remember to check if we do not enter ID or enter one that is not the number
+    if(isNaN(parseID)) return response.status(400).send({msg: "Bad Request. Invalid ID"});
+    const findUser = mockUsers.find((user) => user.id === parseID);
+    if(!findUser) return response.sendStatus(404);
+    return respond.send(findUser);
+}
+);    
+
+// STEP 5: QUERY PARAMS
+//Ex: localhost:3000/products?key=value&key2=value2
+//Ex: http://localhost.3000/api/users?filter=username&value=an => The output would be:
+// { filter: 'username', value: 'an'}
+
+// STEP 6: POST REQUESTS
+app.post('/api/users', (request, response) => {
+    console.log(request.body);
+    const{body} = request; // request của app.post() nằm trong "Thunder Client" 
+    const newUser = {
+        id: mockUsers[mockUsers.length - 1].id+1, ...body // "...body" dùng để gộp các thuộc tính từ body trong request vào trong NewUser
+    }
+    mockUsers.push(newUser);
+    return response.status(201).send(newUser);
+})
 
 // Hàm callback sẽ được gọi lại sau khi server chạy thành công (sẽ thực thi trong tương lai)
 app.listen(PORT, () => {
